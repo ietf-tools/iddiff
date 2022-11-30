1dbd61d428f323c5bae7a05f9413af6a847f7da6
Kesara Rathnayake <kesara@fq.nz>
2022-11-30 13:19:26 +1300

    build: Release 0.4.1

--- iddiff/iddiff.py
+++ iddiff/iddiff.py
@@ -8,7 +8,7 @@ from string import whitespace
 from sys import exit, stderr, stdout
 from tempfile import NamedTemporaryFile
 
-VERSION = '0.4.0'
+VERSION = '0.4.1'
 
 WDIFF_ERROR = 'wdiff functionlity requires GNU Wdiff ' \
               '(https://www.gnu.org/software/wdiff/)\n'
